39b0b12b5d4d87b62fbc88b6ddda9ed492bd4178
Shrabana <shrab81@outlook.com>
2021-11-15 19:15:41 -0800

    CONTROLLER: Home

---
+++ JustLearn.Services.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace JustLearn.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello Mums! This is my first launch!");
+    }
+}
