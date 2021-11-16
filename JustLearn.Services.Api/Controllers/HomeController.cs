3b08487dce54a6e0ddba663ecd26c0f370a1d7a7
Shrabana <shrab81@outlook.com>
2021-11-15 21:40:29 -0800

    CODE RUB: Order Change

--- JustLearn.Services.Api/Controllers/HomeController.cs
+++ JustLearn.Services.Api/Controllers/HomeController.cs
@@ -7,8 +7,8 @@ using RESTFulSense.Controllers;
 
 namespace JustLearn.Services.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class HomeController : RESTFulController
     {
         [HttpGet]
