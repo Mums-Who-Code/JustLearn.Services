5485f24be2fdafe48cd4208211b425e74b4fd9db
Shrabana <shrab81@outlook.com>
2021-12-13 19:44:14 -0800

    BROKER: Insert Profile

--- JustLearn.Services.Api/Brokers/Storages/IStorageBroker.Profile.cs
+++ JustLearn.Services.Api/Brokers/Storages/IStorageBroker.Profile.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using JustLearn.Services.Api.Models.Profiles;
+
 namespace JustLearn.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Profile> InsertProfileAsync(Profile profile);
     }
 }
--- JustLearn.Services.Api/Brokers/Storages/StorageBroker.Profile.cs
+++ JustLearn.Services.Api/Brokers/Storages/StorageBroker.Profile.cs
@@ -2,13 +2,27 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using JustLearn.Services.Api.Models.Profiles;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace JustLearn.Services.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         DbSet<Profile> Profiles { get; set; }
+
+        public async ValueTask<Profile> InsertProfileAsync(Profile profile)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Profile> entityEntry =
+                await broker.Profiles.AddAsync(profile);
+
+            await broker.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
