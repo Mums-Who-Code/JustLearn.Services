0e85d5c23b76cf778b7b8a65f23db1e61eb40bb1
Shrabana <shrab81@outlook.com>
2022-01-17 08:45:40 -0800

    ShouldThrowValidationExceptionOnAddIfProfileIdIsInvalidAndLogItAsync -> FAIL

---
+++ JustLearn.Services.Api/Models/Profiles/Exceptions/InvalidProfileException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace JustLearn.Services.Api.Models.Profiles.Exceptions
+{
+    public class InvalidProfileException : Xeption
+    {
+        public InvalidProfileException()
+            : base(message: "Invalid Profile, fix the error and try again!")
+        { }
+    }
+}
--- JustLearn.Services.Tests.Unit/Services/Foundations/Profiles/ProfileServiceTests.Validations.Add.cs
+++ JustLearn.Services.Tests.Unit/Services/Foundations/Profiles/ProfileServiceTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using JustLearn.Services.Api.Models.Profiles;
 using JustLearn.Services.Api.Models.Profiles.Exceptions;
@@ -33,6 +34,39 @@ namespace JustLearn.Services.Tests.Unit.Services.Foundations.Profiles
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(
                     SameExceptionAs(expectedArtistValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertProfileAsync(It.IsAny<Profile>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfProfileIdIsInvalidAndLogItAsync()
+        {
+            //Given
+            Guid invalidGuid = Guid.Empty;
+            Profile randomProfile = CreateRandomProfile();
+            Profile invalidProfile = randomProfile;
+            invalidProfile.Id = invalidGuid;
+            var invalidProfileException = new InvalidProfileException();
+
+            var expectedProfileValidationException = new ProfileValidationException(invalidProfileException);
+
+            //When
+            ValueTask<Profile> addProfileTask =
+                this.profileService.AddProfileAsync(invalidProfile);
+
+            //Then
+            await Assert.ThrowsAsync<ProfileValidationException>(() =>
+                addProfileTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(
+                    SameExceptionAs(expectedProfileValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
